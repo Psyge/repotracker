d623814f3ecff2b66968322fd118698da1a3a324
Psyge <masto84@gmail.com>
2025-12-01 16:53:52 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -21,10 +21,150 @@ async function getWeather(lat, lon) {
 }
 
 const places = [
-  { name: 'Rovaniemi', lat: 66.50276, lon: 25.72912, url: 'https://visitrovaniemi.fi', icon: 'images/roic.png' },
-  { name: 'Joulupukin Pajakylä', lat: 66.54363, lon: 25.84641, url: 'https://santaclausvillage.info/', icon: 'images/pukki.png', stream: 'https://www.youtube.com/embed/Cp4RRAEgpeU', streamWidth: 320, streamHeight: 180},
-  { name: 'Levi', lat: 67.80886, lon: 24.81125, url: 'https://www.levi.fi/', icon: 'images/levi.png',  stream: 'https://www.youtube.com/embed/X7tdyNFpp1g', streamWidth: 320, streamHeight: 180},
-  { name: 'Ylläs', lat: 67.56501, lon: 24.22361, url: 'https://yllas.fi/', icon: 'images/yllas.png' },
+ { name: 'Rovaniemi', lat: 66.5, lon: 25.7, url: 'https://visitrovaniemi.fi', icon: 'roic.png',  short: 'Rovaniemi on Lapin pääkaupunki ja Joulupukin virallinen kotikaupunki.', 
+     description: `
+        <h2>Rovaniemi – Lapin pääkaupunki ja joulupukin koti</h2>
+
+  <p>
+    Rovaniemi on elinvoimainen kaupunki Suomen Lapissa, aivan Napapiirin tuntumassa. Kaupunki sijaitsee hyvien kulkuyhteyksien varrella – Rovaniemelle pääsee kätevästi sekä lentäen että junalla. Rovaniemeä pidetään yleisesti Lapin pääkaupunkina, ja se toimii koko pohjoisen Suomen tärkeänä hallinnollisena, kaupallisena ja kulttuurisena keskuksena.
+  </p>
+
+  <p>
+    Rovaniemi tunnetaan kansainvälisesti joulupukin virallisena kotipaikkana, mikä tekee siitä yhden Suomen suosituimmista matkailukohteista. Erityisesti talvikuukausina turistit saapuvat ympäri maailmaa kokemaan Lapin taian ja ainutlaatuiset elämykset.
+  </p>
+<p>Yksi Rovaniemellä vierailevien suosikkikokemuksista on Napapiirin ylittäminen. Napapiiri kulkee kaupungin halki, ja sen voi ylittää esimerkiksi Joulupukin Pajakylässä, jossa raja on merkitty selkeästi maastoon. Napapiirin ylitys on monille matkailijoille unohtumaton hetki – se symboloi siirtymistä arktiselle alueelle.</p>
+
+  <h3>Talvella suosituimpia aktiviteetteja on:</h3>
+  <ul>
+    <li>Poro- ja huskyajelut</li>
+    <li>Moottorikelkkasafarit</li>
+    <li>Revontuliretket</li>
+    <li>Laskettelu, lumilautailu ja hiihtäminen Ounasvaaralla</li>
+    <li>Jääveistokset, iglut ja muut lumiaktiviteetit</li>
+    <li>Vierailut Joulupukin Pajakylässä ja Santa Parkissa</li>
+  </ul>
+
+  <h3>Tekemistä ympäri vuoden</h3>
+  <p>
+    Vaikka Rovaniemi on erityisen suosittu talvikohde, tarjoaa kaupunki runsaasti nähtävää ja koettavaa myös muina vuodenaikoina. Rovaniemellä voi tutustua upeisiin luontopoluihin, vierailla mielenkiintoisissa museoissa kuten Arktikum ja Pilke, sekä nauttia vilkkaasta kulttuuri- ja tapahtumatarjonnasta.
+  </p>
+    ` 
+    },
+    { name: 'Joulupukin Pajakylä', lat: 66.54, lon: 25.84, url: 'https://santaclausvillage.info/', icon: 'pukki.png', stream: 'https://www.youtube.com/embed/Cp4RRAEgpeU', streamWidth: 320, streamHeight: 180, short: 'Joulupukin pajakylä - Joulupukin virallinen kotipaikka', description: ` 
+    <h2>Joulupukin Pajakylä – Taianomainen kohtaamispaikka Napapiirillä</h2>
+
+  <p>
+    Joulupukin Pajakylä on yksi Rovaniemen tunnetuimmista nähtävyyksistä. Se sijaitsee aivan Napapiirillä, vain lyhyen matkan päässä keskustasta, ja toimii joulupukin virallisena kohtaamispaikkana ympäri vuoden. Pajakylä on kansainvälinen matkailun vetonaula, jossa vierailee vuosittain tuhansia turisteja eri puolilta maailmaa.
+  </p>
+
+  <h3>Mitä Pajakylässä voi kokea?</h3>
+  <ul>
+    <li>Tapaa itse Joulupukki – Pajakylässä voit tavata Joulupukin henkilökohtaisesti vuoden jokaisena päivänä.</li>
+    <li>Lähetä terveisiä Joulupukin pääpostista – Napapiirillä sijaitsevasta postista voi lähettää kirjeitä erikoisleimalla ja seurata tonttujen postinlajittelua.</li>
+    <li>Ylitä Napapiiri – Pajakylässä kulkee Napapiirin virallinen linja, jonka ylittäminen on monille vierailijoille ikimuistoinen hetki.</li>
+    <li>Tee ostoksia ja nauti Lapin herkuista – Pajakylästä löytyy runsaasti lahjatavarakauppoja sekä tunnelmallisia ravintoloita, joissa voi maistella paikallisia herkkuja.</li>
+    </ul>
+
+  <h3>Talven taikaa</h3>
+  <p>
+    Talvikaudella Pajakylä muuttuu satumaiseksi lumikeitaaksi. Vierailijat voivat nauttia poro- ja huskysafareista, moottorikelkka-ajeluista, jää- ja lumirakennelmista sekä mahdollisesti nähdä revontulet Napapiirin yllä.
+  </p>
+
+  <h3>Auki ympäri vuoden</h3>
+  <p>
+    Joulupukin Pajakylä ei ole vain joulusesongin kohde – se on avoinna joka päivä vuoden ympäri. Kesällä tunnelma on rauhallisempi, ja alueeseen voi tutustua ilman talvikauden suuria kävijämääriä.
+
+  </p>
+
+  <p>
+    Oli vuodenaika mikä tahansa, Joulupukin Pajakylä tarjoaa ikimuistoisen elämyksen kaikenikäisille vierailijoille.
+  </p>`
+    },
+    { name: 'Levi', lat: 67.80, lon: 24.80, url: 'https://www.levi.fi/', icon: 'levi.png', short: 'Levi - Suomen ehkä tunnetuin hiihtokeskus', description: 
+        `
+  <h2>Levi – Suomen tunnetuin hiihtokeskus</h2>
+
+  <p>
+    Levi sijaitsee Kittilässä, Lapissa, ja on yksi Suomen suosituimmista sekä kansainvälisimmistä matkailukohteista. Levi tunnetaan erityisesti laadukkaista rinteistään, monipuolisista aktiviteeteistaan ja vilkkaasta matkailuelämästään. Se on Suomen suurin hiihtokeskus, joka houkuttelee vierailijoita ympäri vuoden.
+  </p>
+
+  <h3>Talven parhaat elämykset</h3>
+  <ul>
+    <li>Hiihto ja lumilautailu: kymmenet rinteet ja huippuluokan latuverkostot tarjoavat vaihtoehtoja kaikentasoisille laskijoille.</li>
+    <li>Alppihiihdon maailmancup: Levi isännöi vuosittain maailmancupin avauskilpailuja.</li>
+    <li>Moottorikelkkasafarit: reittejä Lapin maisemissa niin aloittelijoille kuin kokeneille.</li>
+    <li>Husky- ja poroajelut: unohtumattomia elämyksiä talvisessa luonnossa.</li>
+    <li>Revontuliretket: mahdollisuus kokea pohjoisen taivas täydessä loistossaan.</li>
+    <li>Lumikenkäily ja jäärakennelmat – liikuntaa ja elämyksiä lumen keskellä.</li>
+  </ul>
+
+  <h3>Kesän ja syksyn aktiviteetit</h3>
+  <p>
+    Levi on monipuolinen ympärivuotinen kohde. Kesällä ja ruska-aikaan alue houkuttelee retkeilijöitä, maastopyöräilijöitä, kalastajia ja golfaajia. Levin huipulle pääsee myös gondolihissillä, joka tarjoaa upeat maisemat jokaisena vuodenaikana.
+  </p>
+
+  <h3>Majoitus ja palvelut</h3>
+  <p>
+    Levin kylästä löytyy laaja valikoima majoitusvaihtoehtoja mökeistä laadukkaisiin hotelleihin. Alue tarjoaa myös runsaasti ravintoloita, kahviloita, kauppoja ja viihdepalveluita.
+  </p>
+
+  <h3>Hyvinvointi ja spa-elämykset</h3>
+  <p>
+    Levi tunnetaan myös hyvinvointipalveluistaan – tarjolla on spa-, wellness-, rentoutumis- ja hemmottelupalveluita niin lomailijoille kuin aktiiviurheilijoille.
+  </p>
+
+  <h3>Tapahtumat</h3>
+  <p>
+    Levillä järjestetään ympäri vuoden erilaisia tapahtumia urheilukilpailuista musiikkitapahtumiin. Tunnelma on usein nuorekas, energinen ja kansainvälinen.
+  </p>
+
+`
+},
+    { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png', short: 'Ylläs - Suomen suurin hiihtokeskus', description: `
+  <h2>Ylläs – Tunturien rauhaa ja Lapin upeita elämyksiä</h2>
+
+  <p>
+    Ylläs sijaitsee Kolarin kunnassa, Lapissa, ja se on yksi Suomen 
+    tunnetuimmista talvi- ja luontomatkailukohteista. Alue koostuu kahdesta 
+    tunnelmallisesta kylästä: Äkäslompulosta ja Ylläsjärvestä, jotka tarjoavat 
+    aidon lappilaisen ympäristön ja upeat tunturimaisemat.
+  </p>
+
+  <h3>Mikä tekee Ylläksestä erityisen?</h3>
+
+  <h4>Suomen suurimmat rinne- ja hiihtoalueet</h4>
+  <p>
+    Ylläs tunnetaan laajoista ja monipuolisista laskettelurinteistään. Alueelta löytyy Suomen pisimmät rinteet sekä kattava latuverkosto, josta osa kulkee upeissa Pallas–Yllästunturin kansallispuiston maisemissa.
+  </p>
+
+  <h4>Kansallispuiston läheisyys</h4>
+  <p>
+   Ylläs toimii porttina yhteen Suomen kauneimmista kansallispuistoista. Pallas–Yllästunturin kansallispuisto tarjoaa kymmeniä kilometrejä merkittyjä vaellusreittejä, henkeäsalpaavat ruska- ja kesämaisemat sekä erinomaiset mahdollisuudet retkeilyyn ja marjastukseen.
+  </p>
+
+  <h4>3Talvikauden elämykset</h4>
+ <p>Talvella Ylläksellä voi nauttia monipuolisista aktiviteeteista, kuten husky- ja porosafareista, moottorikelkkailusta, lumikenkäilystä, fatbike-pyöräilystä sekä revontuliretkistä ja valokuvauskursseista.</p>
+
+  <h4>Aito lappilainen hiljaisuus</h4>
+  <p>
+    Ylläs tarjoaa rauhallisen ja luonnonläheisen tunnelman, joka eroaa vilkkaammista Lapin kohteista. Se on täydellinen paikka niille, jotka etsivät hiljaisuutta, puhdasta tunturi-ilmaa ja aitoja lappilaisia elämyksiä.
+  </p>
+
+  <h4>Kesän ja syksyn aktiviteetit</h4>
+  <p>
+    Ylläs on suosittu kohde myös talven ulkopuolella. Kesällä ja ruska-aikaan voi patikoida tuntureilla, pyöräillä monipuolisilla reiteillä, kalastaa, meloa sekä nauttia Lapin yöttömästä yöstä.
+  </p>
+
+  <h4>Kylien palvelut</h4>
+  <p>
+   Äkäslompolon ja Ylläsjärven kylät tarjoavat kattavat palvelut: majoitusvaihtoehtoja mökeistä hotelleihin, ravintoloita, kauppoja, välinevuokraamoja sekä paikallisia tapahtumia ja kulttuuria.
+  </p>
+
+  <p>
+    Ylläs on täydellinen kohde niille, jotka etsivät yhdistelmää luonnon rauhaa, tunturien kauneutta ja monipuolisia aktiviteetteja ympäri vuoden.
+  </p>`
+
+ }
   { name: 'Apukka Resort', lat: 66.578510, lon: 26.014702, url: 'https://apukkaresort.fi/', icon: 'images/apukka.png', stream: 'https://www.youtube.com/embed/bOEvPL206Hc', streamWidth: 320, streamHeight: 180 },
   { name: 'Beautiful Northern Lights live stream. Credits: Starlapland / Samuli Korvanen', lat: 67.41711, lon: 26.58897, url: 'https://repotracker.fi', icon: 'images/iconi.png', stream: 'https://www.youtube.com/embed/dnlQtDad6Dk', streamWidth: 320, streamHeight: 180 }
 ];
@@ -56,18 +196,33 @@ function addMarkers(layer) {
     });
 
     const popupContent = `
-      <strong>${place.name}</strong><br>
-      <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
-      <a href="${place.url}" target="_blank">More info</a>
-      <div class="weather-box" style="margin-top:10px;">
-        <em>Retrieving weather data...</em>
-      </div>
-      ${place.stream ? `<div class="popup-stream" 
-        data-stream="${place.stream}" 
-        data-width="${place.streamWidth || 320}" 
-        data-height="${place.streamHeight || 180}" 
-        style="margin-top:10px;"></div>` : ''}
-    `;
+     <div class="popup-header">
+                <img src="${place.icon}" alt="${place.name}">
+                <strong class="popup-title">${place.name}</strong>
+            </div>
+
+            <div style="font-size: 0.9em; margin: 6px 0; max-width:250px;">
+               ${place.short || place.description}
+            </div>
+
+            <a href="#" class="read-more" data-place="${place.name}">
+   ${translations[currentLang].weather.moreInfo}
+</a>
+
+            <div class="weather-box" style="margin-top:10px;">
+                <em>${translations[currentLang].weather.loading}</em>
+            </div>
+            
+            ${place.stream
+                ? `<div class="popup-stream" 
+                        data-stream="${place.stream}" 
+                        data-width="${place.streamWidth}" 
+                        data-height="${place.streamHeight}" 
+                        style="margin-top:10px;">
+                   </div>`
+                : ''
+            }
+        `;
 
     const marker = L.marker([place.lat, place.lon], { icon: customIcon })
       .bindPopup(popupContent, { className: 'custom-popup' })
@@ -113,7 +268,35 @@ function addMarkers(layer) {
       }
     });
   });
+function showPlaceInfo(place) {
+    const defaultSection = document.getElementById("aurora-default");
+    const infoSection = document.getElementById("place-info");
 
+    // Piilotetaan oletussisältö
+    defaultSection.style.display = "none";
+
+    // Näytetään paikan sisältö
+    infoSection.style.display = "block";
+
+    infoSection.innerHTML = `
+        
+        <p>${place.description || ''}</p>
+        ${place.url ? `<p><a href="${place.url}" target="_blank">Visit website</a></p>` : ''}
+        ${place.stream ? 
+            `<iframe src="${place.stream}" width="100%" height="250" style="border:none;margin-top:10px;"></iframe>` : ''}
+        <button id="back-to-default" style="margin-top:15px;">Takaisin ohjeisiin</button>
+    `;
+
+    // Scrollataan osioon
+    infoSection.scrollIntoView({ behavior: "smooth" });
+
+    // Lisää takaisin-napin toiminto
+    document.getElementById("back-to-default").onclick = () => {
+        infoSection.style.display = "none";
+        defaultSection.style.display = "block";
+        defaultSection.scrollIntoView({ behavior: "smooth" });
+    };
+}
   // Animaatioviive
   document.querySelectorAll('.marker-wrapper').forEach(el => {
     el.style.animationDelay = `${Math.random() * 2}s`;
@@ -124,6 +307,8 @@ function addMarkers(layer) {
 document.addEventListener('DOMContentLoaded', () => {
   setTimeout(initMarkers, 500); // pieni viive varmistaa että map on valmis
 });
+document.addEventListener('languageReady', initMarkers);
+document.addEventListener('mapReady', initMarkers);
 
 
 
